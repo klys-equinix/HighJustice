b1d9e5fbbc777735d5103b62b28922cf4076c19c
Szymon Chmal V3RON <szymonchmal@gmail.com>
2017-12-17 20:02:37 +0100

    MapDrawer fixes

--- src/main/java/com/algorytmy/GUI/Utility/MapDrawer.java
+++ src/main/java/com/algorytmy/GUI/Utility/MapDrawer.java
@@ -1,25 +1,21 @@
 package com.algorytmy.GUI.Utility;
 
-import com.algorytmy.Configuration.Property.ColorProperties;
 import com.algorytmy.Model.Match;
 import javafx.scene.canvas.GraphicsContext;
-import org.springframework.beans.factory.annotation.Autowired;
+import javafx.scene.paint.Color;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 @Component
 public class MapDrawer {
-    @Autowired
-    private ColorProperties colorProperties;
-
     public void drawBoard(GraphicsContext gc, Match.FIELD_VALUE[][] board) {
         /*
         * Assuming board is n by n
         */
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                gc.setFill(colorProperties.getEntityColor(board[i][j]));
+                gc.setFill(getEntityColor(board[i][j]));
                 gc.fillRect(i, j, 1, 1);
             }
         }
@@ -42,5 +38,16 @@ public class MapDrawer {
 
             }
         return board;
+    }
+
+    private Color getEntityColor(Match.FIELD_VALUE fv) {
+        if(fv == Match.FIELD_VALUE.EMPTY)
+            return Color.BLACK;
+        else if(fv == Match.FIELD_VALUE.OBSTACLE)
+            return Color.WHITE;
+        else if(fv == Match.FIELD_VALUE.P1)
+            return Color.RED;
+        else
+            return Color.BLUE;
     }
 }
